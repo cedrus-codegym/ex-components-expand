e46869d591abbda161b53b94c3c9e507853dff59
cedrus172 <cedrusisme@gmail.com>
2022-06-30 15:42:33 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [isExpand, setExpand] = useState(false);
+
+  const expandHandle = () => {
+    setExpand(!isExpand);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header">
+        <h1>Conditional Rendering</h1>
+      </div>
+
+      <div className="content">
+        <button onClick={expandHandle}>
+          {isExpand ? "Đóng giới thiệu" : "Xem giới thiệu"}
+        </button>
+
+        {isExpand ? <ContentExpand /> : ``}
+      </div>
+    </>
   );
 }
+
+const ContentExpand = () => {
+  return (
+    <>
+      <h3>Giới thiệu</h3>
+      <p>
+        Trong ReactJs, đôi khi bạn có một số components và tùy thuộc vào từng
+        điều kiện ví dụ như trạng thái của state, props,... mà bạn muốn hiển thị
+        hoặc một số component nào đó. Khi đó bạn có thể sử dụng Conditional
+        rendering để render ra component mà bạn mong muốn
+      </p>
+    </>
+  );
+};
 
 export default App;
